e20917012160a95125625f99e3138e342e3e491a
Sabitha28 <108917947+Sabitha28@users.noreply.github.com>
2023-12-13 18:16:31 +0530

    Update Baseclass.java

--- src/test/java/stepdefinition/Baseclass.java
+++ src/test/java/stepdefinition/Baseclass.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Baseclass {
 	public static WebDriver driver;
 	public void launchBrowser() throws InterruptedException, IOException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sannaram\\OneDrive - Corebridge Financial\\Documents\\Sabitha\\New_BDD-main\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sannaram\\Documents\\Sabitha\\New_BDD-main\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		 driver = new ChromeDriver(options);
