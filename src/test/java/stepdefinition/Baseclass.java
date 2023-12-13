8ae0e0c639f115a80a2e113ca0fe45ea7cb92572
Sabitha28 <108917947+Sabitha28@users.noreply.github.com>
2023-12-13 18:15:50 +0530

    Update Baseclass.java

--- src/test/java/stepdefinition/Baseclass.java
+++ src/test/java/stepdefinition/Baseclass.java
@@ -4,13 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Baseclass {
-	public static ChromeDriver driver;
-	public void launchBrowser() {
-		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_103.exe");
-		//WebDriver driver = new ChromeDriver();
-		driver=new ChromeDriver();
-		//driver.get("https://www.facebook.com");
-		//return driver;
+	public static WebDriver driver;
+	public void launchBrowser() throws InterruptedException, IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sannaram\\OneDrive - Corebridge Financial\\Documents\\Sabitha\\New_BDD-main\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		 driver = new ChromeDriver(options);
+		String url = Configfilereader.getPath();
+		System.out.println("urll:" + url);
+		driver.get(url);
+		Thread.sleep(2000);
 	}
 
 }
