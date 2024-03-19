398edc16b8606978a07ed5a7c0c33027cdb34de7
Sabitha28 <108917947+Sabitha28@users.noreply.github.com>
2024-03-19 16:41:38 +0530

    Update Baseclass.java

--- src/test/java/stepdefinition/Baseclass.java
+++ src/test/java/stepdefinition/Baseclass.java
@@ -5,15 +5,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Baseclass {
 	public static WebDriver driver;
-	public void launchBrowser() throws InterruptedException, IOException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sannaram\\Documents\\Sabitha\\New_BDD-main\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		 driver = new ChromeDriver(options);
-		String url = Configfilereader.getPath();
-		System.out.println("urll:" + url);
+	public void setup() throws InterruptedException, IOException {
+		
+		//*** chrome version 122.0.6261.129 having issue not able to execute the script ****//
+		//** executed in Edge using webdriver manager without download driver manually **// 
+	//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\sannaram\\OneDrive - Corebridge Financial\\Documents\\Sabitha\\New_BDD-main\\chromedriver.exe");
+		
+EdgeOptions options = new EdgeOptions();		 
+WebDriverManager.edgedriver().setup();
+options.addArguments("--remote-allow-origins=*");
+driver = new EdgeDriver(options);
+Thread.sleep(2000);
+String url = Configfilereader.getPath();
+	System.out.println("urll:" + url);
 		driver.get(url);
 		Thread.sleep(2000);
+		
 	}
 
+
+	
+	
+	public String captureScreenShot(String ScreenshotName) throws IOException {
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        TakesScreenshot tst = (TakesScreenshot) driver;
+        File source = tst.getScreenshotAs(OutputType.FILE);
+        String dest = (System.getProperty("user.dir") + "\\target\\" + ScreenshotName + "_Screenshot.png");
+        File destination = new File(dest);
+        FileHandler.copy(source, destination);
+        return destination.getName();
+    }
 }
+
